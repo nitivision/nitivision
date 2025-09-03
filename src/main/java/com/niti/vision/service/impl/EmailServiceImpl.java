089f740f1c49e9460833fdca34d0e78d934e722a
Aditya <adityadwarehouse@gmail.com>
2025-09-04 02:38:49 +0530

    code changes for forgot password

--- src/main/java/com/niti/vision/service/impl/EmailServiceImpl.java
+++ src/main/java/com/niti/vision/service/impl/EmailServiceImpl.java
@@ -2,23 +2,39 @@ package com.niti.vision.service.impl;
 
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.niti.vision.service.EmailService;
-@Service
-public class EmailServiceImpl implements EmailService{
-	
-    private JavaMailSender javaMailSender;
-    public EmailServiceImpl(JavaMailSender mailSender) {
-        this.javaMailSender = mailSender;
-    }
-    @Override
-    public void sendOtpEmail(String to, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setFrom("nitivisionglobal@gmail.com");
-        message.setSubject("Password Reset OTP - Niti Vision");
-        message.setText("Your OTP for password reset is: " + otp + "\n\nIt is valid for 10 minutes.");
 
-        javaMailSender.send(message);
-    }}
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+	private JavaMailSender javaMailSender;
+
+	public EmailServiceImpl(JavaMailSender mailSender) {
+		this.javaMailSender = mailSender;
+	}
+
+	@Override
+	public void sendOtpEmail(String to, String otp) {
+		System.out.println("Start sendOtpEmail method");
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+			helper.setFrom("nitivisionglobal@gmail.com");
+			helper.setTo(to);
+			helper.setSubject("Password Reset OTP - Niti Vision");
+			helper.setText("Your OTP for password reset is: " + otp + "\n\nIt is valid for 10 minutes.");
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("End sendOtpEmail method");
+		javaMailSender.send(mimeMessage);
+	}
+}
